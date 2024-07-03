7b92ca88bc713d60bbcdaf0d2fb48b677ed63e5f
Folacodes <folacodes@Folacodess-MacBook-Pro.local>
2024-07-03 20:43:34 +0100

    started working on hymns again

---
+++ components/ReadPDF.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+import Pdf from "react-native-pdf";
+
+const ReadPDF = () => {
+  const PdfResource = { uri: "../assets/hymns/", cache: true };
+  return (
+    <View>
+      <Pdf
+        trustAllCerts={false}
+        source={PdfResource}
+        style={{
+          backgroundColor: "red",
+        }}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`number of pages: ${numberOfPages}`);
+        }}
+      />
+    </View>
+  );
+};
+
+export default ReadPDF;
