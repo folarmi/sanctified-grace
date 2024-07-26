a4fdfbbe436b478afbe7754584f423839a7b1818
Folacodes <folacodes@Folacodess-MacBook-Pro.local>
2024-07-26 20:28:03 +0100

    trying to restructure

---
+++ app/(tabs)/resources/psalms/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+const PsalmsLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    ></Stack>
+  );
+};
+
+export default PsalmsLayout;
